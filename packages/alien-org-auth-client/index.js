d5303019ca4833b0043de864d550a4fb7bb08d38
Elvis Tile <elvis19k87@gmail.com>
2026-04-25 03:44:28 +0300

    Refactor authentication client to use JOSE library

--- packages/alien-org-auth-client/index.js
+++ packages/alien-org-auth-client/index.js
@@ -1,15 +1,29 @@
-module.exports = {
-  createAuthClient: () => ({
-    verifyToken: async (token) => {
-      if (!token || token === 'expired') {
-        const { JwtErrors } = require('./index');
-        throw new JwtErrors.JWTExpired();
+const { jwtVerify, createRemoteJWKSet } = require('jose');
+
+const JWKS_URL = 'https://auth.alien.org/.well-known/jwks.json';
+const JWKS = createRemoteJWKSet(new URL(JWKS_URL));
+
+class JOSEError extends Error {}
+class JWTExpired extends JOSEError {}
+
+const JwtErrors = { JOSEError, JWTExpired };
+
+function createAuthClient() {
+  return {
+    async verifyToken(token) {
+      try {
+        const { payload } = await jwtVerify(token, JWKS, {
+          issuer: 'https://auth.alien.org',
+        });
+        if (!payload.sub) throw new JOSEError('Missing sub claim');
+        return { sub: payload.sub };
+      } catch (err) {
+        if (err.code === 'ERR_JWT_EXPIRED') throw new JWTExpired(err.message);
+        if (err instanceof JOSEError) throw err;
+        throw new JOSEError(err.message);
       }
-      return { sub: 'user-' + token.slice(-8) };
-    }
-  }),
-  JwtErrors: {
-    JWTExpired: class extends Error { constructor() { super('JWT Expired'); this.name = 'JWTExpired'; } },
-    JOSEError: class extends Error { constructor() { super('JOSE Error'); this.name = 'JOSEError'; } }
-  }
-};
+    },
+  };
+}
+
+module.exports = { createAuthClient, JwtErrors };
